1fa626bba3098ebc10cdfb6f1d05a7d7bef3a276
Andrei Aciobanitei <andrei@gmail.com>
2018-06-04 01:11:59 +0300

    hyccups service v0.1

--- app/src/main/java/com/samaras/muvi/MyChannel.java
+++ app/src/main/java/com/samaras/muvi/MyChannel.java
@@ -1,4 +1,32 @@
 package com.samaras.muvi;
 
-public class MyChannel {
+import android.util.Log;
+
+import java.util.*;
+
+import ro.pub.acs.hyccups.opportunistic.Channel;
+
+
+public class MyChannel extends Channel {
+    @Override
+    public String getName() { return null; }
+
+    @Override
+    public List getInterests() { return null; }
+
+    @Override
+    public void onPeerConnected(String deviceId, String userId) {
+        Log.v("MyChannel", "onPeerConnected: " + deviceId + "," + userId);
+    }
+
+    @Override
+    public void onPeerDisconnected(String deviceId, String userId) {
+        Log.v("MyChannel", "onPeerDisconnected: " + deviceId + "," + userId);
+    }
+
+    @Override
+    public void onMessageReceived(String sourceUserId, String destinationUserId, String message, long timestamp) { }
+
+    @Override
+    public void onDisseminationReceived(String sourceUserId, String message, long timestamp, String[] tags) { }
 }
