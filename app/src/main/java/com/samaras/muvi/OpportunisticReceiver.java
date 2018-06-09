38e96b985feaa852dc7d7fdc381fec9396e408ed
Andrei Aciobanitei <andrei@gmail.com>
2018-06-09 17:54:34 +0300

    call intent added

--- app/src/main/java/com/samaras/muvi/OpportunisticReceiver.java
+++ app/src/main/java/com/samaras/muvi/OpportunisticReceiver.java
@@ -3,6 +3,7 @@ package com.samaras.muvi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,60 +12,81 @@ import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.samaras.muvi.Activities.ProfileActivity;
+
 import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class OpportunisticReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getStringExtra("action");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        if(action.equalsIgnoreCase("connected")) {
-            String userId = intent.getStringExtra("userId");
-            PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
+        if(user != null) {
+            if(action.equalsIgnoreCase("connected")) {
+                String userId = intent.getStringExtra("userId");
+                Intent resultIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "Your Phone_number"));
+//                Intent resultIntent = new Intent(context, ProfileActivity.class);
+                // Create the TaskStackBuilder and add the intent, which inflates the back stack
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                stackBuilder.addNextIntentWithParentStack(resultIntent);
+                // Get the PendingIntent containing the entire back stack
+                PendingIntent pIntent =
+                        stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+//                PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
 
-            Notification n  = new Notification.Builder(context)
-                    .setContentTitle("MuVi")
-                    .setContentText("User connected to access point with id: " + userId)
-                    .setSmallIcon(R.drawable.icon)
-                    .setContentIntent(pIntent)
-                    .setAutoCancel(true)
-                    .addAction(R.drawable.popcorn, "Call", pIntent)
-                    .addAction(R.drawable.popcorn, "More", pIntent)
-                    .addAction(R.drawable.popcorn, "And more", pIntent).build();
+                Notification n  = new Notification.Builder(context)
+                        .setContentTitle("MuVi")
+                        .setContentText("User connected to access point with id: " + userId)
+                        .setSmallIcon(R.drawable.icon)
+                        .setContentIntent(pIntent)
+                        .setAutoCancel(true)
+                        .addAction(R.drawable.popcorn, "Call", pIntent)
+                        .addAction(R.drawable.popcorn, "Chat", pIntent).build();
 
-            n.defaults |= Notification.DEFAULT_SOUND;
+                n.defaults |= Notification.DEFAULT_SOUND;
 
-            NotificationManager notificationManager =
-                    (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+                NotificationManager notificationManager =
+                        (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
 
-            notificationManager.notify(0, n);
-            // Vibrate the mobile phone
+                notificationManager.notify(0, n);
+                // Vibrate the mobile phone
 //            Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 //            vibrator.vibrate(2000);
-        }
-        else if(action.equalsIgnoreCase("disconnected")) {
-            String userId = intent.getStringExtra("userId");
-            PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
+            }
+            else if(action.equalsIgnoreCase("disconnected")) {
+                String userId = intent.getStringExtra("userId");
+                Intent resultIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "Your Phone_number"));
+//                Intent resultIntent = new Intent(context, ProfileActivity.class);
+                // Create the TaskStackBuilder and add the intent, which inflates the back stack
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                stackBuilder.addNextIntentWithParentStack(resultIntent);
+                // Get the PendingIntent containing the entire back stack
+                PendingIntent pIntent =
+                        stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+//                PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
 
-            Notification n  = new Notification.Builder(context)
-                    .setContentTitle("MuVi")
-                    .setContentText("User disconnected from access point with id: " + userId)
-                    .setSmallIcon(R.drawable.icon)
-                    .setContentIntent(pIntent)
-                    .setAutoCancel(true)
-                    .addAction(R.drawable.popcorn, "Call", pIntent)
-                    .addAction(R.drawable.popcorn, "More", pIntent)
-                    .addAction(R.drawable.popcorn, "And more", pIntent).build();
+                Notification n  = new Notification.Builder(context)
+                        .setContentTitle("MuVi")
+                        .setContentText("User disconnected from access point with id: " + userId)
+                        .setSmallIcon(R.drawable.icon)
+                        .setContentIntent(pIntent)
+                        .setAutoCancel(true)
+                        .addAction(R.drawable.popcorn, "Call", pIntent)
+                        .addAction(R.drawable.popcorn, "Chat", pIntent).build();
 
-            n.defaults |= Notification.DEFAULT_SOUND;
+                n.defaults |= Notification.DEFAULT_SOUND;
 
-            NotificationManager notificationManager =
-                    (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+                NotificationManager notificationManager =
+                        (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
 
-            notificationManager.notify(0, n);
-            // Vibrate the mobile phone
+                notificationManager.notify(0, n);
+                // Vibrate the mobile phone
 //            Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 //            vibrator.vibrate(2000);
+            }
         }
     }
 }
